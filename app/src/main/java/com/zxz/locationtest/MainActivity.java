9ad011a316ef867fb0f8b3e321e32222db3bf213
bayberryan <bayberryan@hotmail.com>
2018-06-12 05:52:47 +0800

    test github clone and update

--- app/src/main/java/com/zxz/locationtest/MainActivity.java
+++ app/src/main/java/com/zxz/locationtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.zxz.locationtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("TAG","Test github clone and update");
     }
 }
